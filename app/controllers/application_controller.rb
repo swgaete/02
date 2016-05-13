10eb7458a326d003f40572ad5b6709239ce36852
swgaete <swgaete@uc.cl>
2016-05-13 17:46:20 -0300

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  def getCantidadTags(tag,ACCESS_TOKEN)
-    hmac = generateHash('GET' + tag.to_s + ACCESS_TOKEN.to_s)
+
+  def parametros
+    tag =params[:tag]
+    access_token = params[:access_token]
+    render {}
+    render json: response, root: false
+
+  end
+  
+  def getCantidadTags
+    tag =params[:tag]
+    access_token = params[:access_token]
     uri= 'https://api.instagram.com/v1/tags/' + tag.to_s + '?access_token=' + ACCESS_TOKEN.to_s
-    return JSON.parse(get(uri, hmac= hmac).body)
-  rescue JSON::ParserError
-    return {}
+    return 
   end
 
   protect_from_forgery with: :exception
