f9eb566ca1c622df5d590bf1bb0bd247ee4db1b4
swgaete <swgaete@uc.cl>
2016-05-14 16:08:17 -0300

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 require 'net/http'
 require 'json'
-require 'instagram'
+
 
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
@@ -8,19 +8,57 @@ class ApplicationController < ActionController::Base
 
 
   def parametros
+
     tag =params[:tag]
     access_token = params[:access_token]
+    uriCantidad= 'https://api.instagram.com/v1/tags/' + tag.to_s + '?access_token=' + access_token.to_s
+    uri= 'https://api.instagram.com/v1/tags/'+ tag.to_s + '/media/recent?access_token='+ access_token.to_s
+    cantidad = get(uriCantidad)
+    media = get(uri)
+    reponse = crearDatos(cantidad, media)
     render {}
     render json: response, root: false
 
   end
-  
-  def getCantidadTags
-    tag =params[:tag]
-    access_token = params[:access_token]
-    uri= 'https://api.instagram.com/v1/tags/' + tag.to_s + '?access_token=' + ACCESS_TOKEN.to_s
-    return 
+
+  def get(uri)
+    uri = URI.parse(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl=true
+    request = Net::HTTP::Get.new(uri.request_uri)
+    respuesta = http.request(request)
+    return JSON.parse(respuest.body)
   end
 
-  protect_from_forgery with: :exception
+  def crearDatos(cantidad, media)
+    respuesta={ "metadata" => {},
+                "posts" => [],
+                "version" => ""
+    }
+    respuesta['metadata']=cantidad['data']['media_count']
+
+    media['data'].each do |item|
+    respuesta['posts']<<{
+      'tags'=> item['tags']
+      'username'=> item['user']['username']
+      'likes'=> item['likes']
+      'url'=> maximaResolucion(item['images']) #ver lo de la calidad
+      'caption' =>  item['caption']['text']
+    }
+    #falta ver lo de la version y lo de la calidad de las imagenes
+    #:Bad_request para lo del error de 400
+    return reponse
+  end
+
+  def maximaResolucion(imagen)
+    if imagen.has_key?('standard_resolution')
+      return imagen['standard_resolution']['url']
+    elsif imagen.has_key?('low_resolution')
+      return imagen['low_resolution']['url']
+    else
+      return imagen['thumbnail']['url']
+    end
+  end
+       
+  # protect_from_forgery with: :exception
 end
--- config/initializers/instagram.rb
+++
@@ -1,5 +0,0 @@
- Instagram.configure do |config|
-  config.client_id     = CLIENT_ID
-  config.client_secret = CLIENT_SECRET
-  config.access_token  = ACCESS_TOKEN
-end
